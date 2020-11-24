cc36cb15b0f60880dde0da00977eb1895cdd6556
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-11-24 19:22:40 -0300

    5. Orientação a Objeto

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from calculadora import Calculadora
+
+calc = Calculadora()
 largura: float = float(input('Qual a largura do cômodo? '))  # Não fixa o tipo da váriável
 profundidade = float(input('Qual a profundidade do cômodo? '))
 ALTURA = 2.9
 RENDIMENTO_TINTA = 10.0
-area_paredes = 2 * (largura + profundidade) * ALTURA
-area_teto = largura * profundidade
 
-print(f'A área das paredes é: {area_paredes}')
-print(f'A litragem de tinta necessária é: {(area_paredes + area_teto) / RENDIMENTO_TINTA}')
+calc.area_paredes = 2 * (largura + profundidade) * ALTURA
+calc.area_teto = largura * profundidade
+
+print(f'A área das paredes é: {calc.area_paredes}')
+print(f'A litragem de tinta necessária é: {(calc.area_paredes + calc.area_teto) / RENDIMENTO_TINTA}')
