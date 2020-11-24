7e4d92f0d3e90e84fb4f0724ed0e9746de17bd08
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-11-24 19:32:20 -0300

    6. Criação de Métodos

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,10 @@
 class Calculadora:
+    ALTURA = 2.9
     area_paredes: float
     area_teto: float
+
+    def calcular_area_paredes(self, largura, profundidade):
+        self.area_paredes = 2 * (largura + profundidade) * self.ALTURA
+
+    def calcular_area_teto(self, largura, profundidade):
+        self.area_teto = largura * profundidade
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ profundidade = float(input('Qual a profundidade do cômodo? '))
 ALTURA = 2.9
 RENDIMENTO_TINTA = 10.0
 
-calc.area_paredes = 2 * (largura + profundidade) * ALTURA
-calc.area_teto = largura * profundidade
+calc.calcular_area_paredes(largura, profundidade)
+calc.calcular_area_teto(largura, profundidade)
 
 print(f'A área das paredes é: {calc.area_paredes}')
-print(f'A litragem de tinta necessária é: {(calc.area_paredes + calc.area_teto) / RENDIMENTO_TINTA}')
+print(f'A litragem de tinta necessária é: {calc.area_teto / RENDIMENTO_TINTA}')
