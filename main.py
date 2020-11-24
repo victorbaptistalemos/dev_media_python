1a10a860a318c73686a00eb1296581f737e97eba
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-11-24 19:15:24 -0300

    4. Realizar os Cálculos da Aplicação

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 largura: float = float(input('Qual a largura do cômodo? '))  # Não fixa o tipo da váriável
 profundidade = float(input('Qual a profundidade do cômodo? '))
 ALTURA = 2.9
+RENDIMENTO_TINTA = 10.0
+area_paredes = 2 * (largura + profundidade) * ALTURA
+area_teto = largura * profundidade
 
-print(f'A área das paredes é: {2 * (largura + profundidade) * ALTURA}')
+print(f'A área das paredes é: {area_paredes}')
+print(f'A litragem de tinta necessária é: {(area_paredes + area_teto) / RENDIMENTO_TINTA}')
